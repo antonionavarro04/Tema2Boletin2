8cf0801f3597d1f7378dd7deb6744354c3234c47
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-10-11 10:56:22 +0200

    first commit

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,54 @@
+package ejercicio1;
+
+// ? Importamos la clase Scanner y Locale
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // ? Programa que pide al Usuario que introduzca una nota del 0 al 10 y el programa le muestre mediante un switch si su nota es INSUFICIENTE, SUFICIENTE, BIEN, NOTABLE o SOBRESALIENTE.
+        // ^ Declaramos la variable nota para usarla con el switch
+        byte nota;
+
+        // ^ Decalaramos la variable read, se la asignamos al Scanner y cambiamos al formato US
+        Scanner read = new Scanner(System.in);
+        read.useLocale(Locale.US);
+        
+        // ! Pedimos al usuario que introduzca su nota, la cual esta comprendida del 1 al 10
+        System.out.print("Introduce tu nota del 0 al 10: ");
+        nota = read.nextByte();
+
+        // ! Comprobamos mediante una estructura Switch la nota introducida
+        switch (nota) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("INSUFICIENTE");
+                break;
+            case 5:
+                System.out.println("SUFICIENTE");
+                break;
+            case 6:
+                System.out.println("BIEN");
+                break;
+            case 7:
+            case 8:
+                System.out.println("NOTABLE");
+                break;
+            case 9:
+            case 10:
+                System.out.println("SOBRESALIENTE");
+                break;
+            default:
+                System.out.println("La nota introducida no es valida");
+                break;
+        }
+        
+        // ^ Cerramos el Scanner
+        read.close();
+
+        // € Hecho por Antonio Navarro
+    }
+}
---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,54 @@
+package ejercicio2;
+
+// ? Importamos la clase Scanner y Locale
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        // ? Programa que pide al Usuario que introduzca un numero del 1 al 7 y el programa le muestre mediante un switch el dia de la semana que corresponde.
+        // ^ Declaramos la variable dia para usarla con el switch
+        byte dia;
+
+        // ^ Decalaramos la variable read, se la asignamos al Scanner y cambiamos al formato US
+        Scanner read = new Scanner(System.in);
+        read.useLocale(Locale.US);
+        
+        // ! Pedimos al usuario que introduzca un numero del 1 al 7
+        System.out.print("Introduce un numero del 1 al 7: ");
+        dia = read.nextByte();
+
+        // ! Comprobamos mediante una estructura Switch a que dia pertenece el numero introducido
+        switch (dia){
+            case 1:
+                System.out.println("El día '1' corresponde a Lunes");
+                break;
+            case 2:
+                System.out.println("El día '2' corresponde a Martes");
+                break;
+            case 3:
+                System.out.println("El día '3' corresponde a Miercoles");
+                break;
+            case 4:
+                System.out.println("El día '4' corresponde a Jueves");
+                break;
+            case 5:
+                System.out.println("El día '5' corresponde a Viernes");
+                break;
+            case 6:
+                System.out.println("El día '6' corresponde a Sabado");
+                break;
+            case 7:
+                System.out.println("El día '7' corresponde a Domingo");
+                break;
+            default:
+                System.out.println("El numero introducido no es valido");
+                break;
+        }
+        
+        // ^ Cerramos el Scanner
+        read.close();
+
+        // € Hecho por Antonio Navarro
+    }
+}
